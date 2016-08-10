cef83d6f2485e304dbe0c172a01594eb70411a05
Krešimir Đuretec <kresimir.duretec@gmail.com>
2016-08-10 13:18:04 +0200

    feature distributions are read from a file

--- DataGenerator/src/benchmarkdp/datagenerator/mutation/Mutator.java
+++ DataGenerator/src/benchmarkdp/datagenerator/mutation/Mutator.java
@@ -1,5 +1,10 @@
 package benchmarkdp.datagenerator.mutation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,17 +16,11 @@ import org.eclipse.m2m.qvt.oml.examples.blackbox.UtilitiesLibrary;
 
 import benchmarkdp.datagenerator.model.PIM.PIMPackage;
 
-
-
 public class Mutator {
 
-	private int[][] featureDist = new int[][] { { 1, 3, 2, 40 }, { 38, 7379, 30, 12923 }, { 3, 105, 7, 1016 },
-			{ 25, 192, 0, 5846 }, { 10, 101, 0, 3143 }, { 79, 2011, 3, 17039 }, { 6, 99, 0, 2966 }, { 1, 10, 0, 175 },
-			{ 123, 399, 0, 62056 }, { 10, 801, 7, 2401 }, { 1, 19, 0, 234 }, { 6, 41, 1, 2621 }, { 1, 10, 0, 260 },
-			{ 10, 52, 0, 3149 }, { 12, 639, 0, 3225 }, { 2, 12, 0, 261 }, { 1, 29, 1, 381 }, { 11, 50, 0, 3139 },
-			{ 2, 27, 0, 666 }, { 21, 140, 0, 4146 } };
-	int n = 20;
-	int m = 4;
+	private int[][] featureDist = null;
+	int n;
+	int m;
 
 	String[] formats = { "doc", "docx" };
 	String[] formatCodes = { "0", "16" };
@@ -33,9 +32,11 @@ public class Mutator {
 
 	private CodeGenerator codeGenerator;
 
-	private String basePath = "file://Users/kresimir/Documents/workspace/MutationOperators/transforms/";
+	private String basePath = "file://Users/kresimir/Projects/TestDataGenerator/TestDataGenerator/MutationOperators/transforms/";
 
 	public Mutator() {
+
+		featureDist = readFeatureDistributions();
 
 		TransformationExecutor.BlackboxRegistry.INSTANCE.registerModule(UtilitiesLibrary.class,
 				"m2m.qvt.oml.ExampleJavaLib", "m2m.qvt.oml");
@@ -47,22 +48,26 @@ public class Mutator {
 
 		mutations = new ArrayList<MutationOperatorInterface>();
 		mutations.add(new MutationOperator("AddPage", basePath + "AddPage.qvto"));
-		
-	/*	MutationOperatorHolder mHolderTC = new MutationOperatorHolder("AddTextContainer");
-		mHolderTC.addMutationOperator(new MutationOperator("AddParagraph", basePath + "AddParagraph.qvto"), 0.85);
-		mHolderTC.addMutationOperator(new MutationOperator("AddTextBox", basePath + "AddTextBox.qvto"), 0.15);
-		mutations.add(mHolderTC);*/
-		
+
+		/*
+		 * MutationOperatorHolder mHolderTC = new
+		 * MutationOperatorHolder("AddTextContainer");
+		 * mHolderTC.addMutationOperator(new MutationOperator("AddParagraph",
+		 * basePath + "AddParagraph.qvto"), 0.85);
+		 * mHolderTC.addMutationOperator(new MutationOperator("AddTextBox",
+		 * basePath + "AddTextBox.qvto"), 0.15); mutations.add(mHolderTC);
+		 */
+
 		mutations.add(new MutationOperator("AddParagraph", basePath + "AddParagraph.qvto"));
 		mutations.add(new MutationOperator("AddTable", basePath + "AddTable.qvto"));
-		
+
 		MutationOperatorHolder mHolder = new MutationOperatorHolder("AddText");
 		mHolder.addMutationOperator(new MutationOperator("AddWord", basePath + "AddWord.qvto"), 0.99);
 		mHolder.addMutationOperator(new MutationOperator("AddHyperLink", basePath + "AddHyperLink.qvto"), 0.01);
 		mutations.add(mHolder);
 		mutations.add(new MutationOperator("ChangeColumnNumber", basePath + "ChangeColumnNumber.qvto"));
 		mutations.add(new MutationOperator("AddTextBox", basePath + "AddTextBox.qvto"));
-		
+
 		docMut = new ArrayList<DocumentMutator>();
 
 		evaluators = new ArrayList<OCLEvaluator>();
@@ -95,10 +100,10 @@ public class Mutator {
 				DocumentMutator dm = new DocumentMutator(
 						new DocumentHolder("Document" + cnt, "PIMs/Document.xmi", formats[f], formatCodes[f]));
 				cnt++;
-				boolean control = false; 
+				boolean control = false;
 				for (int j = 0; j < m; j++) {
-					
-					if (j>0 && control==false) {
+
+					if (j > 0 && control == false) {
 						dm.addMutation(mutations.get(5), 1);
 						control = true;
 					}
@@ -127,9 +132,34 @@ public class Mutator {
 			System.out.println("Done");
 		}
 
-			
 		System.out.println("Transformation done");
 
 	}
 
+	private int[][] readFeatureDistributions() {
+		File f = new File("resources/FeatureDistributions.tsv");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String line = br.readLine();
+			String[] spl = line.split("\t");
+			n = Integer.parseInt(spl[0]);
+			m = Integer.parseInt(spl[1]);
+			line = br.readLine();
+			int[][] mat = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				line = br.readLine();
+				spl = line.split("\t");
+				for (int j = 0; j < m; j++) {
+					mat[i][j] = Integer.parseInt(spl[j+1]);
+				}
+			}
+			return mat;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
