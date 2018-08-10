cb32fbc2228468c65754dd96d37e2cde194a2084
Krešimir Đuretec <kresimir.duretec@gmail.com>
2018-08-10 11:57:02 +0200

    adding logger

--- DataGenerator/src/benchmarkdp/datagenerator/app/Main.java
+++ DataGenerator/src/benchmarkdp/datagenerator/app/Main.java
@@ -1,14 +1,18 @@
 package benchmarkdp.datagenerator.app;
 
-import benchmarkdp.datagenerator.generator.Mutator;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+	private static Logger log = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		Mutator mut = new Mutator();
-		mut.mutate();
+	
+		log.info("Hello World");
+		Configurations configs = new Configurations();
+	
 	}
 
 }
